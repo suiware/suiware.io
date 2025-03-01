2db047465675f40156fb131ca12cb13479c78459
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-03-01 15:21:01 +0100

    Added Recent Projects section to the frontpage.

---
+++ src/content/projects.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: "Sui dApp Starter",
+    description: "'Create React App' for full-stack Sui dApps",
+    url: "https://sui-dapp-starter.dev",
+  },
+  {
+    name: "@suiware/kit",
+    description: "Opinionated React components and hooks for Sui dApps",
+    url: "https://www.npmjs.com/package/@suiware/kit",
+  },
+  {
+    name: "@suiware/ai-tools",
+    description: "Sui tools for Vercel AI SDK",
+    url: "https://npmjs.com/package/@suiware/ai-tools",
+  },
+];
