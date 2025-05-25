e431b895dd3bc507719d0b3d898deb76f0bcafe5
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-05-25 20:28:43 +0200

    Reordered projects.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,13 +5,13 @@ export default [
     url: "https://sui-dapp-starter.dev",
   },
   {
-    name: "@suiware/kit",
-    description: "Opinionated React components and hooks for Sui dApps",
-    url: "https://www.npmjs.com/package/@suiware/kit",
-  },
-  {
     name: "@suiware/ai-tools",
     description: "Sui tools for Vercel AI SDK",
     url: "https://npmjs.com/package/@suiware/ai-tools",
+  },
+  {
+    name: "@suiware/kit",
+    description: "Opinionated React components and hooks for Sui dApps",
+    url: "https://www.npmjs.com/package/@suiware/kit",
   },
 ]
